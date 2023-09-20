361d134b219c9f96af6419f349e42a924ef03410
Lovehansa046 <107402926+Lovehansa046@users.noreply.github.com>
2023-09-20 14:21:37 +0300

    SCRIP-9 final push

--- REACT_PROJECT_1/src/components/app-header/app-header.js
+++ REACT_PROJECT_1/src/components/app-header/app-header.js
@@ -5,7 +5,7 @@ import './app-header.css'
 const AppHeader = ({ liked, allPosts }) => {
     return (
         <div className='app-header d-flex'>
-            <h1> Georg Laabe </h1>
+            <h1> Artjom Plotnikov </h1>
             <h2>Всего записей: {allPosts}, понравилось: {liked}</h2>
         </div>
     )
